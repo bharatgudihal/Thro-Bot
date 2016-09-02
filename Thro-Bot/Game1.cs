a76752b209da9f9d484f59d2e935b5cfd2e0af0b
aarondesin <adesin@ucsc.edu>
2016-09-02 11:48:34 -0600

    Game over hotfix

--- Thro-Bot/Game1.cs
+++ Thro-Bot/Game1.cs
@@ -154,7 +154,6 @@ namespace Thro_Bot
             spawnTimeSpan = TimeSpan.FromSeconds(SPAWN_INTERVAL);
             ui = new UI();
             gameOver = false;
-            playerDeath.Play();
             base.Initialize();
         }
 
@@ -783,6 +782,7 @@ namespace Thro_Bot
                 //Draw the combo indicator
                 spriteBatch.DrawString(ui.gameOverFont, "Replay Y/N?", new Vector2(GraphicsDevice.Viewport.Width/2 - 200, GraphicsDevice.Viewport.Height/2 - 20), Color.White);
                 gameOver = true;
+				playerDeath.Play();
                 gamePaused = true;
             }
 
